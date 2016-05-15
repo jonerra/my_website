dc11bd0340a4710d5bd59d1d4a91efc2c54aaca9
Jon <jonerra23@aim.com>
2016-05-15 17:02:44 +0000

    Add theme

---
+++ core/migrations/0004_test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0003_remove_message_title'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Test',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('subject', models.CharField(max_length=300)),
+            ],
+        ),
+    ]
